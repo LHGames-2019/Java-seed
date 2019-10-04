52437551bb617ce190b7459ea43febbdb0763c5f
Gabriel-Andrew Pollo-Guilbert <gabrielpolloguilbert@gmail.com>
2019-10-03 23:19:44 -0400

    Initial code base for 2019 client

---
+++ src/main/java/io/polyhx/lhgames/Bot.java
@@ -0,0 +1,10 @@
+package io.polyhx.lhgames;
+
+import io.polyhx.lhgames.data.Direction;
+import io.polyhx.lhgames.data.GameInfo;
+
+public class Bot {
+    public Direction getNextDirection(GameInfo info) {
+        return Direction.UP;
+    }
+}
